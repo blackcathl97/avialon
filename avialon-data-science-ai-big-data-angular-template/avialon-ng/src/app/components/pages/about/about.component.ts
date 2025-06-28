0238aa7f7d7105787d77d0ea8f0043da8e1c7dc6
DESKTOP-A2J200S\TNC <blackcathl97@gmail.com>
2025-06-28 15:17:44 +0700

    28.06.2025

--- avialon-data-science-ai-big-data-angular-template/avialon-ng/src/app/components/pages/about/about.component.ts
+++ avialon-data-science-ai-big-data-angular-template/avialon-ng/src/app/components/pages/about/about.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
+  fullText = 'We are BEEWORK AI.\nWe build the future through Data-centric AI.\nWe inspire AI & Human Collaboration.';
+  displayText = '';
+  private currentIndex = 0;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.typeWriter();
+  }
+
+  typeWriter() {
+    if (this.currentIndex < this.fullText.length) {
+      const char = this.fullText.charAt(this.currentIndex);
+      if (char === '\n') {
+        this.displayText += '<br>';
+      } else {
+        this.displayText += char;
+      }
+      this.currentIndex++;
+      setTimeout(() => this.typeWriter(), 40); // tốc độ gõ
+    }
   }
 
 }
